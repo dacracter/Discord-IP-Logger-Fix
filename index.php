489960906154fc6dcd98c74317ea8bb032cf01fa
dacracter <alexander7771@outlook.de>
2024-07-12 14:20:53 +0200

    Created index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Your title</title>
+    </head>
+    <body>
+      <!-- Main Contents of the site -->  
+      <h1>Hello World</h1>
+      <!-- Main PHP include -->
+      <?php include 'main.php'; ?>
+    </body>
+</html>
